7f7e1246f56c22f8d8cd9676335b2e4a7d967d64
micahsantos51 <micahminoru@gmail.com>
2025-10-22 15:39:01 -0700

    finished assignment

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
+
+
 // Get the element with the class "icon"
 let icon = document.getElementsByClassName("icon")[0];
 
 // Add an event listener for the 'click' event on the icon element
 icon.addEventListener('click', responsive_control);
+
+let slideIndex = 1;
+showSlides(slideIndex);
 
 // Function to control the responsiveness of the navigation bar
 function responsive_control() {
@@ -17,4 +22,28 @@ function responsive_control() {
     // If it's not, remove the "responsive" class from the element
     x.className = "topnav";
   }
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
